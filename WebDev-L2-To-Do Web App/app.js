9e3acb4ae1b628f860376048217e47fbd84eeac8
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-07-11 10:43:37 +0530

    all functionlity completed

--- WebDev-L2-To-Do Web App/app.js
+++ WebDev-L2-To-Do Web App/app.js
@@ -1,4 +1,5 @@
 let taskList=[];
+let updateId=0;
 let addTask=(taskName,taskDescription,dt)=>{
   taskList.push({id:taskList.length+1,name:taskName,description:taskDescription,date:dt,status:"Pending"});
   printTaskList();
@@ -19,10 +20,22 @@ let formValidation=()=>{
     {
       if(dateField.value!=="")
       {
-        addTask(nameField.value,descriptionField.value,dateField.value);
-        msg.innerText="Task Created";
-        msg.classList.remove("text-danger");
-        msg.classList.add("text-success");
+        if(updateId==0)
+        {
+          addTask(nameField.value,descriptionField.value,dateField.value);
+          msg.innerText="Task Created";
+          msg.classList.remove("text-danger");
+          msg.classList.add("text-success");
+        }
+        else{
+          updateTask(updateId,nameField.value,descriptionField.value,dateField.value);
+          msg.innerText="Task Updated";
+          msg.classList.remove("text-danger");
+          msg.classList.add("text-success");
+          nameField.value="";
+          descriptionField.value="";
+          dateField.value="";
+        }
       }
       else{
         msg.innerText="Please Enter Date...";
@@ -46,13 +59,13 @@ const printTaskList=()=>{
   completeListContainer.innerHTML="";
   taskList.forEach((task,index)=>{
     if(task.status==="Pending")
-      printPendingList(task);
+      printPendingList(task,index);
     if(task.status==="Completed")
-      printCompletedList(task);
+      printCompletedList(task,index);
   });
 }
 //Pending List
-const printPendingList=(task)=>{
+const printPendingList=(task,index)=>{
   const pendingListContainer=document.getElementById("pending-list");
   let container=document.createElement("div");
   container.classList.add("d-flex", "flex-column", "align-items-center", "justify-content-center", "bg-pending", "p-2", "rounded-2");
@@ -62,16 +75,16 @@ const printPendingList=(task)=>{
       <div>${task.description}</div>
       <div>${task.date}</div>
       <div class="d-flex gap-2">
-        <button class="btn btn-danger">Delete</button>
-        <button class="btn btn-primary">Edit</button>
-        <button class="btn btn-success">Complete</button>
+        <button class="btn btn-danger" onclick=deleteTask(${index})>Delete</button>
+        <button class="btn btn-primary" onclick=editTask(${task.id})>Edit</button>
+        <button class="btn btn-success" onclick=toggleStatus(${task.id})>Complete</button>
       </div>
     `;
     container.innerHTML=newPendingTask;
   pendingListContainer.append(container);
 }
 //Completed List
-const printCompletedList=(task)=>{
+const printCompletedList=(task,index)=>{
   const completeListContainer=document.getElementById("completed-list");
   let container=document.createElement("div");
   container.classList.add("d-flex", "flex-column", "align-items-center", "justify-content-center", "bg-success", "p-2", "rounded-2");
@@ -81,9 +94,9 @@ const printCompletedList=(task)=>{
       <div>${task.description}</div>
       <div>${task.date}</div>
       <div class="d-flex gap-2">
-        <button class="btn btn-danger">Delete</button>
-        <button class="btn btn-primary">Edit</button>
-        <button class="btn btn-warning">Incomplete</button>
+        <button class="btn btn-danger" onclick=deleteTask(${index})>Delete</button>
+        <button class="btn btn-primary" onclick=editTask(${task.id})>Edit</button>
+        <button class="btn btn-warning" onclick=toggleStatus(${task.id})>Incomplete</button>
       </div>
     `;
     container.innerHTML=newCompletedTask;
@@ -91,3 +104,59 @@ const printCompletedList=(task)=>{
   completeListContainer.append(container);
 }
 printTaskList();
+
+
+let deleteTask=(index)=>{
+  taskList.splice(index,1);
+  let msg=document.getElementById("msg");
+  msg.innerText="Task Deleted";
+  msg.classList.add("text-success");
+  printTaskList();
+}
+
+let editTask=(id)=>{
+  let nameField=document.getElementById("taskName");
+  let descriptionField=document.getElementById("taskDesc");
+  let dateField=document.getElementById("taskDate");
+  let msg=document.getElementById("msg");
+  let _task=taskList.find(x=>x.id==id);
+  if(_task!=null)
+  {
+    let updateBtn=document.getElementById("update-btn");
+    let addBtn=document.getElementById("add-btn");
+    nameField.value=_task.name;
+    descriptionField.value=_task.description;
+    dateField.value=_task.date;
+    updateId=_task.id;
+    nameField.focus();
+    updateBtn.hidden=false;
+    addBtn.hidden=true;
+  }
+}
+
+let updateTask=(id,name,desc,date)=>{
+  let updateBtn=document.getElementById("update-btn");
+  let addBtn=document.getElementById("add-btn");
+  let _task=taskList.find(x=>x.id==id);
+  if(_task!=null)
+  {
+    _task.name=name;
+    _task.description=desc;
+    _task.date=date;
+  }
+  updateBtn.hidden=true;
+  addBtn.hidden=false;
+  printTaskList();
+}
+
+let toggleStatus=(id)=>{
+  let _task=taskList.find(x=>x.id==id);
+  if (_task) {
+    if (_task.status == "Pending") {
+      _task.status = "Completed";
+    } else {
+      _task.status = "Pending";
+    }
+    printTaskList();
+  }
+}
